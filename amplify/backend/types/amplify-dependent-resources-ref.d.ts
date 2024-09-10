c9d189ec4796c6879d94ffe64ac02bcbc5c38526
AbdullahBinHasanHydrus <Abdullah.hasan@hydrusdigitalbd.com>
2024-09-10 17:46:13 +0600

    initial push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "abdamplifyf1e7bd48": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
